19ab4084f54702c2cc53bd41e90da3f997ebd45c
John <johnsdiyplayground@gmail.com>
2018-08-26 16:38:59 -0400

    Use this file to upload to your Arduino
    It waits for the RD command from Bolt, then sends temp and humidity data
    back to it using software serial

---
+++ bolt-javascript-code-for-DHT22-example.js
@@ -0,0 +1,9 @@
+setChartTitle('Johns DIY Playground')
+var lineGraph1 = new boltGraph();
+lineGraph1.setChartType("lineGraph");
+lineGraph1.setAxisName('Time','Temp F');
+lineGraph1.plotChart('time_stamp','temp');
+var lineGraph2 = new boltGraph();
+lineGraph2.setChartType("lineGraph");
+lineGraph2.setAxisName('Time','Humidity %');
+lineGraph2.plotChart('time_stamp','hum');
